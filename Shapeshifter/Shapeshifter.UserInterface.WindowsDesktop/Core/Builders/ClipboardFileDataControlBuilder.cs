479bbe8901105507cb776cc96bc70f94fc27b66c
Mathias Lykkegaard Lorenzen <mathias.lorenzen@live.com>
2015-05-03 12:06:54 +0200

    forgot to add a few missing files.

---
+++ Shapeshifter/Shapeshifter.UserInterface.WindowsDesktop/Core/Builders/ClipboardFileDataControlBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Shapeshifter.Core.Data;
+using Shapeshifter.Core.Factories;
+using Shapeshifter.UserInterface.WindowsDesktop.Controls.Clipboard;
+
+namespace Shapeshifter.UserInterface.WindowsDesktop.Core.Builders
+{
+    class ClipboardFileDataControlBuilder : IClipboardDataControlFactory<ClipboardFileDataControl>
+    {
+        public ClipboardFileDataControl Build(IClipboardData data)
+        {
+            return new ClipboardFileDataControl()
+            {
+                DataContext = data
+            };
+        }
+
+        public bool CanBuild(IClipboardData data)
+        {
+            return data is ClipboardFileData;
+        }
+    }
+}
