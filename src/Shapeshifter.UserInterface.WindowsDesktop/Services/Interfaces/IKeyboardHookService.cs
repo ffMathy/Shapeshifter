902b1b3a7a60932cda7c416f80dee97ec7862b28
Mathias Lykkegaard Lorenzen <mathias.lorenzen@live.com>
2015-05-03 17:37:39 +0200

    removed unused imports.

--- src/Shapeshifter.UserInterface.WindowsDesktop/Services/Interfaces/IKeyboardHookService.cs
+++ src/Shapeshifter.UserInterface.WindowsDesktop/Services/Interfaces/IKeyboardHookService.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Shapeshifter.UserInterface.WindowsDesktop.Services.Events;
 
 namespace Shapeshifter.UserInterface.WindowsDesktop.Services.Interfaces
