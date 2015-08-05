c26632351b16140cac6b551748cc5783bac24e16
ffMathy <mathias.lorenzen@live.com>
2015-08-05 19:26:33 +0200

    started adding actions to shapeshifter.

---
+++ src/Shapeshifter.Core/Actions/IAction.cs
@@ -0,0 +1,15 @@
+﻿using Shapeshifter.Core.Data;
+
+namespace Shapeshifter.Core.Actions
+{
+    public interface IAction
+    {
+        string Title { get; }
+
+        string Description { get; }
+
+        bool CanPerform(IClipboardData clipboardData);
+
+        void Perform(IClipboardData clipboardData);
+    }
+}
---
+++ src/Shapeshifter.UserInterface.WindowsDesktop/Actions/PasteAsPlainTextAction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Shapeshifter.Core.Actions;
+using Shapeshifter.Core.Data;
+
+namespace Shapeshifter.UserInterface.WindowsDesktop.Actions
+{
+    class PasteAsPlainTextAction : IAction
+    {
+        public string Description
+        {
+            get
+            {
+                return "Pastes clipboard contents as plain text.";
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return "Paste as plain text";
+            }
+        }
+
+        public bool CanPerform(IClipboardData clipboardData)
+        {
+            return clipboardData is ClipboardTextData;
+        }
+
+        public void Perform(IClipboardData clipboardData)
+        {
+            var textData = (ClipboardTextData)clipboardData;
+
+            throw new NotImplementedException();
+        }
+    }
+}
