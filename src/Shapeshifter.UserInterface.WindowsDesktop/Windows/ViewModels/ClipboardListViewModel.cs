89980e7dc64015f822e43a5fc86546d18127ab78
ffMathy <mathias.lorenzen@live.com>
2015-08-07 21:17:22 +0200

    fix for high dpi scaling issues.

--- src/Shapeshifter.UserInterface.WindowsDesktop/Windows/ViewModels/ClipboardListViewModel.cs
+++ src/Shapeshifter.UserInterface.WindowsDesktop/Windows/ViewModels/ClipboardListViewModel.cs
@@ -14,13 +14,28 @@ namespace Shapeshifter.UserInterface.WindowsDesktop.Windows.ViewModels
     class ClipboardListViewModel : INotifyPropertyChanged
     {
         private IClipboardControlDataPackage selectedElement;
+        private IAction selectedAction;
 
         private readonly IEnumerable<IAction> allActions;
 
         public ObservableCollection<IClipboardControlDataPackage> Elements { get; private set; }
         public ObservableCollection<IAction> Actions { get; private set; }
 
-        public IAction SelectedAction { get; set; }
+        public IAction SelectedAction
+        {
+            get
+            {
+                return selectedAction;
+            }
+            set
+            {
+                selectedAction = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(SelectedAction)));
+                }
+            }
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -60,16 +75,21 @@ namespace Shapeshifter.UserInterface.WindowsDesktop.Windows.ViewModels
         private void SetActions()
         {
             Actions.Clear();
+            SelectedAction = null;
 
             if (selectedElement != null)
             {
                 foreach (var data in selectedElement.Contents)
                 {
-                    foreach(var action in allActions)
+                    foreach (var action in allActions)
                     {
-                        if(action.CanPerform(data))
+                        if (action.CanPerform(data))
                         {
                             Actions.Add(action);
+                            if (SelectedAction == null)
+                            {
+                                SelectedAction = action;
+                            }
                         }
                     }
                 }
@@ -95,6 +115,7 @@ namespace Shapeshifter.UserInterface.WindowsDesktop.Windows.ViewModels
         private void Service_ControlAdded(object sender, Services.Events.ControlEventArgument e)
         {
             Elements.Insert(0, e.Package);
+            SelectedElement = e.Package;
         }
     }
 }
