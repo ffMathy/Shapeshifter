0cdf53ddf2db5f2a3c500f9a59bfd7d3e3af5a2b
ffMathy <mathias.lorenzen@live.com>
2015-08-15 20:19:55 +0200

    added missing test file.

---
+++ src/Shapeshifter.Tests/Controls/Clipboard/ViewModels/ClipboardFileDataViewModelTest.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using Shapeshifter.Core.Data.Interfaces;
+using Shapeshifter.UserInterface.WindowsDesktop.Controls.Clipboard.ViewModels.Text.Interfaces;
+
+namespace Shapeshifter.Tests.Controls.Clipboard.ViewModels.Text
+{
+    [TestClass]
+    public class ClipboardFileDataViewModelTest : TestBase
+    {
+    }
+}
