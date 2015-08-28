d5bd08ac74bc114310cfa68dce81781da87d792c
ffMathy <mathias.lorenzen@live.com>
2015-08-28 07:25:30 +0200

    attempt at resolving file factory issues.

--- src/Shapeshifter.UserInterface.WindowsDesktop/Factories/FileClipboardDataControlFactory.cs
+++ src/Shapeshifter.UserInterface.WindowsDesktop/Factories/FileClipboardDataControlFactory.cs
@@ -12,6 +12,7 @@ using Shapeshifter.UserInterface.WindowsDesktop.Services.Api;
 using Shapeshifter.UserInterface.WindowsDesktop.Handles.Factories.Interfaces;
 using System.Text;
 using System.Collections.Generic;
+using Shapeshifter.UserInterface.WindowsDesktop.Handles.Interfaces;
 
 namespace Shapeshifter.UserInterface.WindowsDesktop.Factories
 {
@@ -40,25 +41,32 @@ namespace Shapeshifter.UserInterface.WindowsDesktop.Factories
 
         public IClipboardControl BuildControl(IClipboardData clipboardData)
         {
-            if (clipboardData is IClipboardFileCollectionData)
+            if (clipboardData 
+                is IClipboardFileCollectionData)
             {
-                return clipboardFileCollectionControlFactory.CreateControl((IClipboardFileCollectionData)clipboardData);
+                return clipboardFileCollectionControlFactory.CreateControl(
+                    (IClipboardFileCollectionData)clipboardData);
             }
-            else if (clipboardData is IClipboardFileData)
+            else if (clipboardData 
+                is IClipboardFileData)
             {
-                return clipboardFileControlFactory.CreateControl((IClipboardFileData)clipboardData);
+                return clipboardFileControlFactory.CreateControl(
+                    (IClipboardFileData)clipboardData);
             }
             else
             {
-                throw new ArgumentException("Unknown clipboard data type.", nameof(clipboardData));
+                throw new ArgumentException(
+                    "Unknown clipboard data type.", nameof(clipboardData));
             }
         }
 
-        public IClipboardData BuildData(uint format, byte[] rawData)
+        public IClipboardData BuildData(
+            uint format, byte[] rawData)
         {
             if (!CanBuildData(format))
             {
-                throw new ArgumentException("Can't construct data from this format.", nameof(format));
+                throw new ArgumentException(
+                    "Can't construct data from this format.", nameof(format));
             }
 
             var files = GetFilesCopiedFromRawData(rawData);
@@ -71,16 +79,29 @@ namespace Shapeshifter.UserInterface.WindowsDesktop.Factories
             using (var memoryHandle = memoryHandleFactory.AllocateInMemory(data))
             {
                 var count = ClipboardApi.DragQueryFile(memoryHandle.Pointer, uint.MaxValue, null, 0);
-                for (var i = 0u; i < count; i++)
+                if (count == 0)
                 {
-                    var filenameBuilder = new StringBuilder(256);
-                    ClipboardApi.DragQueryFile(memoryHandle.Pointer, i, filenameBuilder, filenameBuilder.Capacity);
-
-                    files.Add(filenameBuilder.ToString());
+                    throw new InvalidOperationException("No files were in the clipboard.");
                 }
+
+                FetchFilesFromMemory(files, memoryHandle, count);
             }
 
             return files;
+        }
+
+        static void FetchFilesFromMemory(List<string> files, IMemoryHandle memoryHandle, int count)
+        {
+            for (var i = 0u; i < count; i++)
+            {
+                var length = ClipboardApi.DragQueryFile(memoryHandle.Pointer, i, null, 0);
+                var filenameBuilder = new StringBuilder(length);
+
+                length = ClipboardApi.DragQueryFile(memoryHandle.Pointer, i, filenameBuilder, length + 1);
+
+                var fileName = filenameBuilder.ToString();
+                files.Add(fileName.Substring(0, length));
+            }
         }
 
         IClipboardData ConstructDataFromFiles(
