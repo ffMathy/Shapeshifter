95ccfc31d274511434b93c2f2da77efad328e360
ffMathy <mathias.lorenzen@live.com>
2015-08-28 22:04:36 +0200

    async filter added.

---
+++ src/Shapeshifter.UserInterface.WindowsDesktop/Infrastructure/Threading/AsyncFilter.cs
@@ -0,0 +1,55 @@
+﻿using Shapeshifter.UserInterface.WindowsDesktop.Infrastructure.Threading.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shapeshifter.UserInterface.WindowsDesktop.Infrastructure.Threading
+{
+    class AsyncFilter : IAsyncFilter
+    {
+        public async Task<IEnumerable<TResult>> FilterAsync<TResult>(IEnumerable<Task<TResult>> candidatesTask, Func<TResult, bool> filter)
+        {
+            return await FilterAsync(candidatesTask, result => Task.FromResult(filter(result)));
+        }
+
+        public async Task<IEnumerable<TResult>> FilterAsync<TResult>(IEnumerable<TResult> candidatesTask, Func<TResult, Task<bool>> filter)
+        {
+            return await FilterAsync(candidatesTask.Select(result => Task.FromResult(result)), filter);
+        }
+
+        public async Task<IEnumerable<TResult>> FilterAsync<TResult>(IEnumerable<Task<TResult>> candidatesTask, Func<TResult, Task<bool>> filter)
+        {
+            var candidates = await Task.WhenAll(candidatesTask);
+            var validations = await GetValidationsAsync(filter, candidates);
+
+            return FilterUsingValidations(candidates, validations);
+        }
+
+        static async Task<bool[]> GetValidationsAsync<TResult>(Func<TResult, Task<bool>> filter, TResult[] candidates)
+        {
+            var validationsTask = new List<Task<bool>>();
+            foreach (var candidate in candidates)
+            {
+                validationsTask.Add(filter(candidate));
+            }
+
+            var validations = await Task.WhenAll(validationsTask);
+            return validations;
+        }
+
+        static IEnumerable<TResult> FilterUsingValidations<TResult>(TResult[] candidates, bool[] validations)
+        {
+            var results = new List<TResult>();
+            for (var i = 0; i < validations.Length; i++)
+            {
+                if (validations[i])
+                {
+                    results.Add(candidates[i]);
+                }
+            }
+
+            return results;
+        }
+    }
+}
