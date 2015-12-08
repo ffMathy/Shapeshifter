9acf57d2c8c26b47e0d1c22b6f8b429322a32dcb
ffMathy <mathias.lorenzen@live.com>
2015-12-08 21:06:36 +0100

    even more test coverage.

---
+++ src/Shapeshifter.Tests/Web/DomainNameResolverTest.cs
@@ -0,0 +1,68 @@
+﻿namespace Shapeshifter.UserInterface.WindowsDesktop.Web
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Autofac;
+
+    using Mediators.Interfaces;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using NSubstitute;
+
+    using Services.Web.Interfaces;
+
+    [TestClass]
+    public class DomainNameResolverTest: TestBase
+    {
+        [TestMethod]
+        [TestCategory("Integration")]
+        public async Task CanResolveGoogleDomain()
+        {
+            var container = CreateContainer();
+
+            var resolver = container.Resolve<IDomainNameResolver>();
+
+            var ipAddresses = await resolver.GetDomainIpAddressesAsync("google.com");
+            var firstIpAddress = ipAddresses.First();
+
+            Assert.IsTrue(firstIpAddress
+                .ToString()
+                .StartsWith("195.249."));
+        }
+
+        [TestMethod]
+        [TestCategory("Integration")]
+        public async Task CanDetectGoogleAsValidDomain()
+        {
+            var container = CreateContainer();
+
+            var resolver = container.Resolve<IDomainNameResolver>();
+            var isValid = await resolver.IsValidDomainAsync("google.com");
+            Assert.IsTrue(isValid);
+        }
+
+        [TestMethod]
+        [TestCategory("Integration")]
+        public async Task IsNotValidWhenUsingGarbageText()
+        {
+            var container = CreateContainer();
+
+            var resolver = container.Resolve<IDomainNameResolver>();
+            var isValid = await resolver.IsValidDomainAsync("hello world foobar");
+            Assert.IsFalse(isValid);
+        }
+
+        [TestMethod]
+        [TestCategory("Integration")]
+        public async Task IsNotValidWithInvalidDomain()
+        {
+            var container = CreateContainer();
+
+            var resolver = container.Resolve<IDomainNameResolver>();
+            var isValid = await resolver.IsValidDomainAsync("fooooooooooooooooooo.com");
+            Assert.IsFalse(isValid);
+        }
+    }
+}
