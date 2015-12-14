199944fb044be617e01c6c42006bcfe8cabc5e66
ffMathy <mathias.lorenzen@live.com>
2015-12-14 17:08:04 +0100

    new tests.

---
+++ src/Shapeshifter.Tests/Services/Messages/Interceptors/ClipboardCopyInterceptorTest.cs
@@ -0,0 +1,54 @@
+﻿namespace Shapeshifter.WindowsDesktop.Services.Messages.Interceptors
+{
+    using System;
+
+    using Autofac;
+
+    using Interfaces;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Native.Interfaces;
+
+    using NSubstitute;
+
+    [TestClass]
+    public class ClipboardCopyInterceptorTest: TestBase
+    {
+        [TestMethod]
+        public void InstallAddsClipboardFormatListener()
+        {
+            var container = CreateContainer(
+                c => {
+                    c.RegisterFake<IClipboardNativeApi>()
+                     .AddClipboardFormatListener(new IntPtr(1337))
+                     .Returns(true);
+                    c.RegisterFake<IWindowNativeApi>();
+                });
+
+            var interceptor = container.Resolve<IClipboardCopyInterceptor>();
+            interceptor.Install(new IntPtr(1337));
+
+            var fakeClipboardNativeApi = container.Resolve<IClipboardNativeApi>();
+            fakeClipboardNativeApi
+                .Received()
+                .AddClipboardFormatListener(new IntPtr(1337));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void InstallThrowsExceptionIfAddingClipboardFormatListenerFailed()
+        {
+            var container = CreateContainer(
+                c => {
+                    c.RegisterFake<IClipboardNativeApi>()
+                     .AddClipboardFormatListener(new IntPtr(1337))
+                     .Returns(false);
+                    c.RegisterFake<IWindowNativeApi>();
+                });
+
+            var interceptor = container.Resolve<IClipboardCopyInterceptor>();
+            interceptor.Install(new IntPtr(1337));
+        }
+    }
+}
--- src/Shapeshifter.Tests/Structures/FormatDataPairTest.cs
+++ src/Shapeshifter.Tests/Structures/FormatDataPairTest.cs
@@ -12,7 +12,7 @@
         public void CanConstructFormatDataPair()
         {
             var item = new FormatDataPair(1337, new byte[] { 1, 2, 3 });
-            Assert.AreEqual(1337, item.Format);
+            Assert.AreEqual(1337u, item.Format);
             Assert.AreEqual(1, item.Data[0]);
             Assert.AreEqual(2, item.Data[1]);
             Assert.AreEqual(3, item.Data[2]);
