02d935713017bdc9c43bb2b0f97c6f6e3214c0c5
ffMathy <mathias.lorenzen@live.com>
2015-12-14 07:27:27 +0100

    more coverage.

---
+++ src/Shapeshifter.Tests/Data/Actions/PinClipboardDataActionTest.cs
@@ -0,0 +1,59 @@
+﻿namespace Shapeshifter.WindowsDesktop.Data.Actions
+{
+    using System.Threading.Tasks;
+
+    using Autofac;
+
+    using Data.Interfaces;
+
+    using Interfaces;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using NSubstitute;
+
+    using Services.Files;
+    using Services.Files.Interfaces;
+
+    [TestClass]
+    public class PinClipboardDataActionTest : ActionTestBase
+    {
+        [TestMethod]
+        public async Task CanNotPerformWithEmptyData()
+        {
+            var container = CreateContainer();
+
+            var fakeData = Substitute.For<IClipboardDataPackage>();
+
+            var action = container.Resolve<IPinClipboardDataAction>();
+            Assert.IsFalse(await action.CanPerformAsync(fakeData));
+        }
+
+        [TestMethod]
+        public void CanReadTitle()
+        {
+            var container = CreateContainer();
+
+            var action = container.Resolve<IPinClipboardDataAction>();
+            Assert.IsNotNull(action.Title);
+        }
+
+        [TestMethod]
+        public void CanReadDescription()
+        {
+            var container = CreateContainer();
+
+            var action = container.Resolve<IPinClipboardDataAction>();
+            Assert.IsNotNull(action.Description);
+        }
+
+        [TestMethod]
+        public void OrderIsCorrect()
+        {
+            var container = CreateContainer();
+
+            var action = container.Resolve<IPinClipboardDataAction>();
+            Assert.AreEqual(200, action.Order);
+        }
+    }
+}
