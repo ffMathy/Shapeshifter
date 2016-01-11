d6a59fc2e3d45755fb797f866bf8419d8ef2f507
ffMathy <mathias.lorenzen@live.com>
2016-01-11 07:10:06 +0100

    added missing classes.

---
+++ src/Shapeshifter.WindowsDesktop/Services/Arguments/Interfaces/ISingleArgumentProcessor.cs
@@ -0,0 +1,9 @@
+﻿namespace Shapeshifter.WindowsDesktop.Services.Arguments.Interfaces
+{
+    public interface ISingleArgumentProcessor : IArgumentProcessor
+    {
+        bool CanProcess(string[] arguments);
+
+        void Process(string[] arguments);
+    }
+}
