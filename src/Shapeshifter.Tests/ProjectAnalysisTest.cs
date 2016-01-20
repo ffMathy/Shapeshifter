062861ec634ecc58e3a5029d1589d031f6fe285c
ffMathy <mathias.lorenzen@live.com>
2016-01-20 08:22:33 +0100

    more tests.

---
+++ src/Shapeshifter.Tests/ProjectAnalysisTest.cs
@@ -0,0 +1,21 @@
+﻿namespace Shapeshifter.WindowsDesktop
+{
+    using System;
+    using System.ComponentModel;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ProjectAnalysisTest: TestBase
+    {
+        [TestMethod]
+        [Category("Integration")]
+        public void ProjectFilesShouldCopyAllReferences()
+        {
+            var root = Extensions.GetProjectRoot();
+            var projectsToTest
+
+            throw new NotImplementedException();
+        }
+    }
+}
