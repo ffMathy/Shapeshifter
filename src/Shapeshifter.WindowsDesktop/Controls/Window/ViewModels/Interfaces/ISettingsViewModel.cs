fe85513137d04bef41a3c4163a3185c82b5e0030
ffMathy <mathias.lorenzen@live.com>
2016-01-20 08:05:27 +0100

    settings window view models are done.

---
+++ src/Shapeshifter.WindowsDesktop/Controls/Window/ViewModels/Interfaces/ISettingsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapeshifter.WindowsDesktop.Controls.Window.ViewModels.Interfaces
+{
+    public interface ISettingsViewModel: INotifyPropertyChanged
+    {
+        bool StartWithWindows { get; set; }
+    }
+}
