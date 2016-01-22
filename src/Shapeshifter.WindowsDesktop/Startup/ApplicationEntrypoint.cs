200aaad7d47386b29e3d627f7e7fa178d9c1bccd
ffMathy <mathias.lorenzen@live.com>
2016-01-22 18:38:29 +0100

    added missing operations.

---
+++ src/Shapeshifter.WindowsDesktop/Startup/ApplicationEntrypoint.cs
@@ -0,0 +1,44 @@
+﻿namespace Shapeshifter.WindowsDesktop.Startup
+{
+    using System.Threading.Tasks;
+
+    using Infrastructure.Dependencies.Interfaces;
+
+    using Interfaces;
+
+    public class ApplicationEntrypoint: ISingleInstance
+    {
+        readonly IStartupPreparationOperation startupPreparationOperation;
+        readonly IPostPreparationOperation postPreparationOperation;
+        readonly IMainWindowPreparationOperation mainWindowPreparationOperation;
+
+        public ApplicationEntrypoint(
+            IStartupPreparationOperation startupPreparationOperation,
+            IPostPreparationOperation postPreparationOperation,
+            IMainWindowPreparationOperation mainWindowPreparationOperation)
+        {
+            this.startupPreparationOperation = startupPreparationOperation;
+            this.postPreparationOperation = postPreparationOperation;
+            this.mainWindowPreparationOperation = mainWindowPreparationOperation;
+        }
+
+        public async Task Start(
+            params string[] arguments)
+        {
+            await Prepare(arguments);
+            if (startupPreparationOperation.ShouldTerminate)
+            {
+                return;
+            }
+
+            await postPreparationOperation.RunAsync();
+            await mainWindowPreparationOperation.RunAsync();
+        }
+
+        async Task Prepare(string[] arguments)
+        {
+            startupPreparationOperation.Arguments = arguments;
+            await startupPreparationOperation.RunAsync();
+        }
+    }
+}
---
+++ src/Shapeshifter.WindowsDesktop/Startup/Interfaces/IStartupPreparationOperation.cs
@@ -0,0 +1,9 @@
+﻿namespace Shapeshifter.WindowsDesktop.Startup.Interfaces
+{
+    public interface IStartupPreparationOperation : IStartupOperation
+    {
+        bool ShouldTerminate { get; }
+
+        string[] Arguments { get; set; }
+    }
+}
