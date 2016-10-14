cc7bb2cb73b8f0ec68f814c784bc83b59dae76f1
Mathias Lorenzen <mathias.lorenzen@live.com>
2016-10-15 01:39:35 +0200

    color picker buttons added.

--- src/Shapeshifter.WindowsDesktop/Controls/Setup/ViewModels/ColorPickerViewModel.cs
+++ src/Shapeshifter.WindowsDesktop/Controls/Setup/ViewModels/ColorPickerViewModel.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Shapeshifter.WindowsDesktop.Controls.Setup.ViewModels.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Shapeshifter.WindowsDesktop.Controls.Window.ViewModels.Interfaces;
+using Shapeshifter.WindowsDesktop.Controls.Window.ViewModels;
 
 namespace Shapeshifter.WindowsDesktop.Controls.Setup.ViewModels
 {
-    class ColorPickerViewModel
+    class ColorPickerViewModel : IColorPickerViewModel
     {
+        public IUserInterfaceViewModel UserInterfaceViewModel { get; private set; }
+
+        public ColorPickerViewModel(
+            IUserInterfaceViewModel userInterfaceViewModel)
+        {
+            UserInterfaceViewModel = userInterfaceViewModel;
+        }
     }
 }
--- src/Shapeshifter.WindowsDesktop/Controls/Setup/ViewModels/Interfaces/IColorPickerViewModel.cs
+++ src/Shapeshifter.WindowsDesktop/Controls/Setup/ViewModels/Interfaces/IColorPickerViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shapeshifter.WindowsDesktop.Controls.Window.ViewModels.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Shapeshifter.WindowsDesktop.Controls.Setup.ViewModels.Interfaces
 {
     public interface IColorPickerViewModel
     {
+        IUserInterfaceViewModel UserInterfaceViewModel { get; }
     }
 }
