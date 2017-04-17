bd32201e2b7499f1b4598a76ce881aa8b0ae0a02
Mathias Lorenzen <mathias.lorenzen@live.com>
2017-04-17 21:30:18 +0200

    app component stuff.

--- src/Shapeshifter.Website/wwwroot/out-tsc/main.js
+++
@@ -1,9 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
-import { AppModule } from './app/app.module';
-if (environment.production) {
-    enableProdMode();
-}
-platformBrowserDynamic().bootstrapModule(AppModule);
-//# sourceMappingURL=C:/Users/mathi/Source/Repos/Shapeshifter/src/Shapeshifter.Website/wwwroot/Client/main.js.map
