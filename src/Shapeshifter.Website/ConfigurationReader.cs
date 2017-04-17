dcf70e4da98976c048ce7a6ffa772b3ea031c068
Mathias Lorenzen <mathias.lorenzen@live.com>
2017-04-17 19:44:13 +0200

    progress

---
+++ src/Shapeshifter.Website/ConfigurationReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shapeshifter.Website
+{
+    using System.IO;
+
+    using Microsoft.Extensions.Configuration;
+
+    public class ConfigurationReader
+    {
+        readonly IConfigurationRoot _configuration;
+
+        public ConfigurationReader(string path)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(path)
+                .AddEnvironmentVariables("shapeshifter:");
+
+            _configuration = builder.Build();
+        }
+
+        public string Read(string key)
+        {
+            return _configuration[key];
+        }
+    }
+}
--- src/Shapeshifter.Website/Controllers/PatreonController.cs
+++ src/Shapeshifter.Website/Controllers/PatreonController.cs
@@ -6,21 +6,43 @@ namespace Shapeshifter.Website.Controllers
     using System.Net;
     using System.Net.Http;
 
+    using Microsoft.ApplicationInsights.AspNetCore.Extensions;
+
+    using Newtonsoft.Json;
+
     public class PatreonController : Controller
     {
+        readonly ConfigurationReader _secretReader;
+
+        public PatreonController()
+        {
+            _secretReader = new ConfigurationReader("secrets.json");
+        }
+
         public async Task<IActionResult> Postback(
-            string code, 
+            string code,
             string state)
         {
-            var clientId = "TODO";
-            var clientSecret = "TODO";
-            var redirectUri = "TODO";
+            var clientId = _secretReader.Read("patreon:clientId");
+            var clientSecret = _secretReader.Read("patreon:clientSecret");
+
+            var redirectUri = HttpContext
+                .Request
+                .GetUri();
+            var redirectUriString = redirectUri
+                .ToString()
+                .Replace(redirectUri.Query, "");
+
             using (var client = new HttpClient())
             {
-                var response = await client.PostAsync("https://api.patreon.com/oauth2/token?code=" + WebUtility.UrlEncode(code) + "&grant_type=authorization_code&client_id=" + clientId + "&client_secret=" + clientSecret + "&redirect_uri=" + WebUtility.UrlEncode(redirectUri), null);
+                var response = await client.PostAsync("https://api.patreon.com/oauth2/token?code=" + WebUtility.UrlEncode(code) + "&grant_type=authorization_code&client_id=" + clientId + "&client_secret=" + clientSecret + "&redirect_uri=" + WebUtility.UrlEncode(redirectUriString), null);
                 var json = await response.Content.ReadAsStringAsync();
+
+                var jsonObject = JsonConvert.DeserializeObject<dynamic>(json);
+                var accessToken = jsonObject.access_token;
+
+                return Redirect;
             }
-            return null;
         }
     }
 }
