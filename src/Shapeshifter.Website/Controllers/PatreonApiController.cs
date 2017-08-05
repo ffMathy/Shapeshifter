808462ccf479a60f3e2c9d2394d4d9153c7af4a8
Mathias Lorenzen <mathias.lorenzen@live.com>
2017-08-05 13:47:02 +0200

    packages config.

--- src/Shapeshifter.Website/Controllers/PatreonApiController.cs
+++ src/Shapeshifter.Website/Controllers/PatreonApiController.cs
@@ -4,45 +4,48 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Shapeshifter.Website.Controllers
 {
-    [Produces("application/json")]
-    [Route("api/patreon")]
-    public class PatreonApiController : Controller
+  [Produces("application/json")]
+  [Route("api/patreon")]
+  public class PatreonApiController : Controller
+  {
+    private readonly IPatreonClient _client;
+
+    public PatreonApiController(
+      IConfigurationReader configurationReader)
     {
-    public PatreonApiController()
-    {
-      _client = new PatreonClient();
+      _client = new PatreonClient(configurationReader.Read("patreon.creatorsAccessToken"));
     }
 
-        // GET: api/Patreon
-        [HttpGet]
-        public IEnumerable<string> GetSupporters()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/Patreon/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-        
-        // POST: api/Patreon
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-        
-        // PUT: api/Patreon/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-        
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+    // GET: api/Patreon
+    [HttpGet]
+    public IEnumerable<string> GetSupporters()
+    {
+      return new string[] { "value1", "value2" };
     }
+
+    // GET: api/Patreon/5
+    [HttpGet("{id}", Name = "Get")]
+    public string Get(int id)
+    {
+      return "value";
+    }
+
+    // POST: api/Patreon
+    [HttpPost]
+    public void Post([FromBody]string value)
+    {
+    }
+
+    // PUT: api/Patreon/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody]string value)
+    {
+    }
+
+    // DELETE: api/ApiWithActions/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+  }
 }
