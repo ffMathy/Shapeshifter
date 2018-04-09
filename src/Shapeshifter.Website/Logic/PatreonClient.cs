3e417a0be993862a819a1251d4bf8b94060c7b5c
Mathias Lorenzen <mathias.lorenzen@live.com>
2018-04-09 20:47:52 +0200

    pledges fixed.

--- src/Shapeshifter.Website/Logic/PatreonClient.cs
+++ src/Shapeshifter.Website/Logic/PatreonClient.cs
@@ -7,27 +7,29 @@ namespace Shapeshifter.Website
 {
 	using FluffySpoon.Http;
 	using Models;
-  using System.Net.Http;
+	using System.Net.Http;
 
-  public class PatreonClient : IPatreonClient
-  {
-	readonly IRestClient _restClient;
+	public class PatreonClient : IPatreonClient
+	{
+		readonly IRestClient _restClient;
 
-    const int campaignId = 557794;
+		const int campaignId = 557794;
 
-    readonly string accessToken;
+		readonly string accessToken;
 
-    public PatreonClient(
-        string accessToken)
-    {
-      this.accessToken = accessToken;
+		public PatreonClient(
+			string accessToken)
+		{
+			this.accessToken = accessToken;
 
-	  _restClient = new RestClient();
-    }
+			_restClient = new RestClient();
+		}
 
-    public async Task<IEnumerable<Pledge>> GetPledges()
-    {
-		_restClient.GetAsync<DataWrapper<Pledge>>();
-    }
-  }
+		public async Task<IEnumerable<Pledge>> GetPledges()
+		{
+			var pledges = await _restClient.GetAsync<DataWrapper<Pledge[]>>(
+				new Uri("https://www.patreon.com/api/oauth2/api/campaigns/" + campaignId + "/pledges?include=patron"));
+			return pledges.Data;
+		}
+	}
 }
