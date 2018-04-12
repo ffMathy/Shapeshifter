565fd7c1f7fe6ffc615a88c8b61640969b6eed7f
Mathias Lorenzen <mathias.lorenzen@live.com>
2018-04-12 20:40:50 +0200

    appveyor stuff.

--- src/Shapeshifter.Tests/Services/ProcessManagerTest.cs
+++ src/Shapeshifter.Tests/Services/ProcessManagerTest.cs
@@ -10,36 +10,6 @@
     [TestClass]
     public class ProcessManagerTest: UnitTestFor<IProcessManager>
     {
-        [Ignore]
-        [TestMethod]
-        [TestCategory("Integration")]
-        public void CanLaunchProcessesAndKillThemAfterDisposing()
-        {
-            var initialRunningProcesses = GetTimeoutProcesses()
-                .Length;
-            using (SystemUnderTest)
-            {
-                SystemUnderTest.LaunchCommand("timeout", "/t -1 /nobreak");
-
-                var runningProcessesBeforeDisposal = GetTimeoutProcesses()
-                    .Length;
-                Extensions.AssertWait(
-                    () =>
-                    Assert.AreEqual(initialRunningProcesses + 1, runningProcessesBeforeDisposal));
-            }
-
-            var runningProcessesAfterDisposal = GetTimeoutProcesses()
-                .Length;
-            Extensions.AssertWait(
-                () =>
-                Assert.AreEqual(initialRunningProcesses, runningProcessesAfterDisposal));
-        }
-
-        static Process[] GetTimeoutProcesses()
-        {
-            return Process.GetProcessesByName("timeout");
-        }
-
         [TestMethod]
         [ExpectedException(typeof (Win32Exception))]
         public void ThrowsExceptionWhenLaunchingFileThatDoesNotExist()
