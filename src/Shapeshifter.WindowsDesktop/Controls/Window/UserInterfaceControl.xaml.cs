b351672657588bc458863a967a230c0319a90c67
Mathias Lorenzen <mathias.lorenzen@live.com>
2018-04-13 15:49:42 +0200

    highlighting bug when switching between packages and action view has been resolved.

--- src/Shapeshifter.WindowsDesktop/Controls/Window/UserInterfaceControl.xaml.cs
+++ src/Shapeshifter.WindowsDesktop/Controls/Window/UserInterfaceControl.xaml.cs
@@ -1,15 +1,58 @@
-﻿using System.Windows.Controls;
+﻿using Shapeshifter.WindowsDesktop.Controls.Window.ViewModels.Interfaces;
+using Shapeshifter.WindowsDesktop.Infrastructure.Events;
+using Shapeshifter.WindowsDesktop.Mediators.Interfaces;
+using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Shapeshifter.WindowsDesktop.Controls.Window
 {
-    /// <summary>
-    /// Interaction logic for UserInterfaceControl.xaml
-    /// </summary>
-    public partial class UserInterfaceControl : UserControl
+	/// <summary>
+	/// Interaction logic for UserInterfaceControl.xaml
+	/// </summary>
+	[TemplatePart(Name = "Core", Type = typeof(FrameworkElement))]
+	[TemplateVisualState(Name = "InPackagesList", GroupName = "TargetList")]
+	[TemplateVisualState(Name = "InActionList", GroupName = "TargetList")]
+	public partial class UserInterfaceControl : UserControl, IDisposable
     {
-        public UserInterfaceControl()
+		private IUserInterfaceViewModel viewModel;
+
+		public UserInterfaceControl()
         {
             InitializeComponent();
-        }
-    }
+			VisualStateManager.GoToElementState(this, "InPackagesList", true);
+		}
+
+		public void Dispose()
+		{
+			viewModel.UserInterfacePaneSwapped -= ViewModel_UserInterfacePaneSwapped;
+		}
+
+		public void Initialize(IUserInterfaceViewModel viewModel)
+		{
+			viewModel.UserInterfacePaneSwapped += ViewModel_UserInterfacePaneSwapped;
+			DataContext = viewModel;
+
+			this.viewModel = viewModel;
+		}
+
+		protected override void OnInitialized(EventArgs e)
+		{
+			base.OnInitialized(e);
+		}
+
+		private void ViewModel_UserInterfacePaneSwapped(object sender, UserInterfacePaneSwappedEventArgument e)
+		{
+			switch (e.Pane)
+			{
+				case ClipboardUserInterfacePane.Actions:
+					VisualStateManager.GoToElementState(this, "InActionList", true);
+					break;
+
+				case ClipboardUserInterfacePane.ClipboardPackages:
+					VisualStateManager.GoToElementState(this, "InPackagesList", true);
+					break;
+			}
+		}
+	}
 }
