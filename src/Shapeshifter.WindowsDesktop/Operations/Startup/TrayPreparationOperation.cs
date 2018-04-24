02d6e6a22602292e2c956b67d5e1d962f93efa39
Mathias Lorenzen <mathias.lorenzen@live.com>
2018-04-24 21:30:25 +0200

    log file made easier.

--- src/Shapeshifter.WindowsDesktop/Operations/Startup/TrayPreparationOperation.cs
+++ src/Shapeshifter.WindowsDesktop/Operations/Startup/TrayPreparationOperation.cs
@@ -1,53 +1,63 @@
 ﻿namespace Shapeshifter.WindowsDesktop.Operations.Startup
 {
-    using System.Threading.Tasks;
-    using System.Windows.Forms;
+	using System.Threading.Tasks;
+	using System.Windows.Forms;
 
-    using Controls.Window.Factories.Interfaces;
+	using Controls.Window.Factories.Interfaces;
 
-    using Infrastructure.Events;
+	using Infrastructure.Events;
 
-    using Interfaces;
+	using Interfaces;
 
-    using Services.Interfaces;
+	using Services.Interfaces;
+	using Shapeshifter.WindowsDesktop.Services.Files;
+	using Shapeshifter.WindowsDesktop.Services.Files.Interfaces;
+	using Shapeshifter.WindowsDesktop.Services.Processes.Interfaces;
+	using Application = System.Windows.Application;
 
-    using Application = System.Windows.Application;
+	public class TrayPreparationOperation : ITrayPreparationOperation
+	{
+		readonly ITrayIconManager trayIconManager;
+		readonly ISettingsWindowFactory settingsWindowFactory;
+		readonly IProcessManager processManager;
 
-    public class TrayPreparationOperation: ITrayPreparationOperation
-    {
-        readonly ITrayIconManager trayIconManager;
-        readonly ISettingsWindowFactory settingsWindowFactory;
+		public TrayPreparationOperation(
+			ITrayIconManager trayIconManager,
+			ISettingsWindowFactory settingsWindowFactory,
+			IProcessManager processManager)
+		{
+			this.trayIconManager = trayIconManager;
+			this.settingsWindowFactory = settingsWindowFactory;
+			this.processManager = processManager;
+		}
 
-        public TrayPreparationOperation(
-            ITrayIconManager trayIconManager,
-            ISettingsWindowFactory settingsWindowFactory)
-        {
-            this.trayIconManager = trayIconManager;
-            this.settingsWindowFactory = settingsWindowFactory;
-        }
-
-        public async Task RunAsync()
-        {
+		public async Task RunAsync()
+		{
 			var assemblyName = typeof(TrayPreparationOperation).Assembly.GetName();
 
 			trayIconManager.IconClicked += TrayIconManager_IconClicked;
 			trayIconManager.UpdateHoverText("Shapeshifter version " + assemblyName.Version);
-            trayIconManager.UpdateMenuItems(
-                "Settings",
-                new[]
-                {
-                    new MenuItem(
-                    "Exit",
-                    (sender, e) => Application.Current.Shutdown())
-                });
+			trayIconManager.UpdateMenuItems(
+				"Settings",
+				new[]
+				{
+					new MenuItem(
+						"Exit",
+						(sender, e) => Application.Current.Shutdown()),
+					new MenuItem(
+						"Show log file",
+						(sender, e) => processManager.LaunchFile(
+							FileManager.GetFullPathFromTemporaryPath(
+								"Shapeshifter.log")))
+				});
 
 			trayIconManager.DisplayInformation("Shapeshifter is ready", "You can now use Shapeshifter for managing your clipboard.");
 		}
 
-        void TrayIconManager_IconClicked(object sender, TrayIconClickedEventArgument e)
-        {
-            var window = settingsWindowFactory.Create();
-            window.Show();
-        }
-    }
+		void TrayIconManager_IconClicked(object sender, TrayIconClickedEventArgument e)
+		{
+			var window = settingsWindowFactory.Create();
+			window.Show();
+		}
+	}
 }
