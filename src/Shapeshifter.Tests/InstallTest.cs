adb8e4d0e355fe925ebf26d305047d29edac8f6b
Mathias Lorenzen <mathias.lorenzen@live.com>
2018-04-24 23:41:28 +0200

    new crash test.

--- src/Shapeshifter.Tests/InstallTest.cs
+++ src/Shapeshifter.Tests/InstallTest.cs
@@ -1,17 +1,44 @@
 ﻿using System;
 using System.ComponentModel;
+using System.IO;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Shapeshifter.WindowsDesktop
 {
 	[TestClass]
-	public class InstallTest: TestBase
+	public class InstallTest : TestBase
 	{
 		[TestMethod]
 		[Category("Integration")]
 		public void CanInstallShapeshifter()
 		{
+			var directory = Environment.CurrentDirectory;
+			Console.WriteLine("Working directory: " + directory);
 			
+			var rootPath = FindRootPathFromPath(directory);
+			Console.WriteLine("Root path: " + rootPath);
+
+			var applicationBuildPath = Path.Combine(rootPath, "build", "application");
+			foreach(var file in Directory.GetFiles(applicationBuildPath)) {
+				if(Path.GetFileName(file) != "Shapeshifter.exe") 
+					continue;
+
+				Console.WriteLine("Deleting file " + file);
+				File.Delete(file);
+			}
+
+			Thread.Sleep(1000);
+
+			Assert.AreEqual(1, Directory.GetFiles(applicationBuildPath).Length);
+		}
+
+		public string FindRootPathFromPath(string path) {
+			var readmeFile = Path.Combine(path, "README.md");
+			if(File.Exists(readmeFile))
+				return path;
+			
+			return FindRootPathFromPath(new DirectoryInfo(path).Parent.FullName);
 		}
 	}
 }
