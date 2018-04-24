64e99640bdc2a342af27388fd1f4ac43939ed81c
Mathias Lorenzen <mathias.lorenzen@live.com>
2018-04-24 23:49:55 +0200

    flushing of tests.

--- src/Shapeshifter.Tests/UnitTestFor.cs
+++ src/Shapeshifter.Tests/UnitTestFor.cs
@@ -1,131 +1,131 @@
 ﻿namespace Shapeshifter.WindowsDesktop
 {
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
 
-    using Autofac;
+	using Autofac;
 
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using Serilog;
+	using Services.Files.Interfaces;
 
-    using Services.Files.Interfaces;
-    
-    public abstract class UnitTestFor<TSystemUnderTest>: TestBase
-        where TSystemUnderTest : class
-    {
+	public abstract class UnitTestFor<TSystemUnderTest> : TestBase
+		where TSystemUnderTest : class
+	{
 
-        ILifetimeScope container;
+		ILifetimeScope container;
 
-        /// <summary>
-        /// Represents the container, which has the class of the unit's dependencies automatically registered as fakes. You can use the <see cref="IncludeFakeFor{T}"/> and <see cref="ExcludeFakeFor{T}"/> method to exclude or include fakes other than the ones automatically registered.
-        /// </summary>
-        protected ILifetimeScope Container
-        {
-            get
-            {
-                Setup();
-                return container;
-            }
-        }
+		/// <summary>
+		/// Represents the container, which has the class of the unit's dependencies automatically registered as fakes. You can use the <see cref="IncludeFakeFor{T}"/> and <see cref="ExcludeFakeFor{T}"/> method to exclude or include fakes other than the ones automatically registered.
+		/// </summary>
+		protected ILifetimeScope Container
+		{
+			get
+			{
+				Setup();
+				return container;
+			}
+		}
 
-        TSystemUnderTest systemUnderTest;
+		TSystemUnderTest systemUnderTest;
 
-        /// <summary>
-        /// Represents the class of the unit being tested, with all its dependencies registered as fakes in the container automatically. You can use the <see cref="IncludeFakeFor{T}"/> and <see cref="ExcludeFakeFor{T}"/> method to exclude or include fakes other than the ones automatically registered.
-        /// </summary>
-        protected TSystemUnderTest SystemUnderTest
-        {
-            get
-            {
-                Setup();
-                return systemUnderTest;
-            }
-        }
+		/// <summary>
+		/// Represents the class of the unit being tested, with all its dependencies registered as fakes in the container automatically. You can use the <see cref="IncludeFakeFor{T}"/> and <see cref="ExcludeFakeFor{T}"/> method to exclude or include fakes other than the ones automatically registered.
+		/// </summary>
+		protected TSystemUnderTest SystemUnderTest
+		{
+			get
+			{
+				Setup();
+				return systemUnderTest;
+			}
+		}
 
-        protected TSystem Get<TSystem>()
-        {
-            return Container.Resolve<TSystem>();
-        }
+		protected TSystem Get<TSystem>()
+		{
+			return Container.Resolve<TSystem>();
+		}
 
-        readonly List<Type> fakeExceptions;
-        readonly List<Type> fakeInclusions;
+		readonly List<Type> fakeExceptions;
+		readonly List<Type> fakeInclusions;
 
-        protected UnitTestFor()
-        {
-            fakeExceptions = new List<Type>();
-            fakeInclusions = new List<Type>();
-        }
+		protected UnitTestFor()
+		{
+			fakeExceptions = new List<Type>();
+			fakeInclusions = new List<Type>();
+		}
 
-        protected void ExcludeFakeFor<T>()
-        {
-            fakeExceptions.Add(typeof(T));
-        }
+		protected void ExcludeFakeFor<T>()
+		{
+			fakeExceptions.Add(typeof(T));
+		}
 
-        protected void IncludeFakeFor<T>()
-        {
-            fakeInclusions.Add(typeof(T));
-        }
+		protected void IncludeFakeFor<T>()
+		{
+			fakeInclusions.Add(typeof(T));
+		}
 
-        void Setup()
-        {
-            if ((container != null) && (systemUnderTest != null))
-            {
-                return;
-            }
+		void Setup()
+		{
+			if ((container != null) && (systemUnderTest != null))
+			{
+				return;
+			}
 
-            container = CreateContainerWithFakeDependencies(
-                (c) => {
-                    foreach (var fake in fakeInclusions)
-                    {
-                        Extensions.RegisterFake(c, fake);
-                    }
-                },
-                fakeExceptions.ToArray());
-            systemUnderTest = container.Resolve<TSystemUnderTest>();
-        }
+			container = CreateContainerWithFakeDependencies(
+				(c) => {
+					foreach (var fake in fakeInclusions)
+					{
+						Extensions.RegisterFake(c, fake);
+					}
+				},
+				fakeExceptions.ToArray());
+			systemUnderTest = container.Resolve<TSystemUnderTest>();
+		}
 
-        [TestCleanup]
-        public void Cleanup()
-        {
-            var fileManager = Container.Resolve<IFileManager>();
-            var folder = fileManager.PrepareIsolatedFolder();
-            if (!string.IsNullOrEmpty(folder))
-            {
-                Directory.Delete(folder, true);
-            }
+		[TestCleanup]
+		public void Cleanup()
+		{
+			Log.CloseAndFlush();
 
-			try { 
-				var temporaryPath = Path.Combine(
-					Path.GetTempPath(),
-					"Shapeshifter");
-				if (Directory.Exists(temporaryPath)) {
-					Directory.Delete(temporaryPath, true);
-				}
-			} catch { }
+			var fileManager = Container.Resolve<IFileManager>();
+			var folder = fileManager.PrepareIsolatedFolder();
+			if (!string.IsNullOrEmpty(folder))
+			{
+				Directory.Delete(folder, true);
+			}
 
-            fakeExceptions.Clear();
-            fakeInclusions.Clear();
+			var temporaryPath = Path.Combine(
+				Path.GetTempPath(),
+				"Shapeshifter");
+			if (Directory.Exists(temporaryPath))
+			{
+				Directory.Delete(temporaryPath, true);
+			}
 
-            Extensions.ClearCache();
-            DisposeContainer();
-        }
+			fakeExceptions.Clear();
+			fakeInclusions.Clear();
 
-        void DisposeContainer()
-        {
-            container?.Dispose();
-            container = null;
-        }
+			Extensions.ClearCache();
+			DisposeContainer();
+		}
 
-        static ILifetimeScope CreateContainerWithFakeDependencies(
-            Action<ContainerBuilder> setupCallback,
-            params Type[] exceptTypes)
-        {
-            return CreateContainer(
-                c =>
-                {
-                    c.RegisterFakesForDependencies<TSystemUnderTest>(exceptTypes);
-                    setupCallback(c);
-                });
-        }
-    }
+		void DisposeContainer()
+		{
+			container?.Dispose();
+			container = null;
+		}
+
+		static ILifetimeScope CreateContainerWithFakeDependencies(
+			Action<ContainerBuilder> setupCallback,
+			params Type[] exceptTypes)
+		{
+			return CreateContainer(
+				c => {
+					c.RegisterFakesForDependencies<TSystemUnderTest>(exceptTypes);
+					setupCallback(c);
+				});
+		}
+	}
 }
