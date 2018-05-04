413c0b4836c3ee40221d4df1e263e280d7a37053
Mathias Lykkegaard Lorenzen <mathias.lorenzen@live.com>
2018-05-04 14:57:42 +0200

    Update InstallArgumentProcessor.cs

--- src/Shapeshifter.WindowsDesktop/Services/Arguments/InstallArgumentProcessor.cs
+++ src/Shapeshifter.WindowsDesktop/Services/Arguments/InstallArgumentProcessor.cs
@@ -30,7 +30,6 @@
 
 		readonly IProcessManager processManager;
 		readonly ICertificateManager certificateManager;
-		readonly ISignHelper signHelper;
 		readonly IEnvironmentInformation environmentInformation;
 		readonly ISettingsViewModel settingsViewModel;
 		readonly IThreadDelay threadDelay;
@@ -42,7 +41,6 @@
 		public InstallArgumentProcessor(
 			IProcessManager processManager,
 			ICertificateManager certificateManager,
-			ISignHelper signHelper,
 			IEnvironmentInformation environmentInformation,
 			ISettingsViewModel settingsViewModel,
 			IThreadDelay threadDelay,
@@ -50,7 +48,6 @@
 		{
 			this.processManager = processManager;
 			this.certificateManager = certificateManager;
-			this.signHelper = signHelper;
 			this.environmentInformation = environmentInformation;
 			this.settingsViewModel = settingsViewModel;
 			this.threadDelay = threadDelay;
@@ -114,10 +111,8 @@
 		void Install()
 		{
 			PrepareInstallDirectory();
-			
 			InstallToInstallDirectory();
-			SignAssembly();
-
+			
 			ConfigureDefaultSettings();
 
 			Logger.Information("Default settings have been configured.");
@@ -127,18 +122,6 @@
 				processManager.GetCurrentProcessFilePath());
 
 			Logger.Information("Launched installed executable.");
-		}
-
-		void SignAssembly()
-		{
-			var selfSignedCertificate = InstallCertificateIfNotFound();
-			signHelper.SignAssemblyWithCertificate(
-				TargetExecutableFile,
-				selfSignedCertificate);
-
-			threadDelay.Execute(1000);
-
-			Logger.Information("Executable signed with newly created self-signing certificate.");
 		}
 
 		void InstallToInstallDirectory()
