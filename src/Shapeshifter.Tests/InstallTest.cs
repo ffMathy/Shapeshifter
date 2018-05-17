5bd5586ee1bf5cae8c59732cc24d11664dc19239
Mathias Lykkegaard Lorenzen <mathias.lorenzen@live.com>
2018-05-17 09:22:36 +0200

    Update InstallTest.cs

--- src/Shapeshifter.Tests/InstallTest.cs
+++ src/Shapeshifter.Tests/InstallTest.cs
@@ -133,7 +133,7 @@ namespace Shapeshifter.WindowsDesktop
 			var logFilePath = FileManager.GetFullPathFromTemporaryPath($"Shapeshifter{DateTime.Now:yyyyMMdd}.log");
 			File.Copy(logFilePath, "Log.txt", true);
 
-			var logOutput = File.ReadAllLines("Log.txt");
+			var logOutput = File.ReadAllText("Log.txt")-Replace("\r", "").Split("\n\n");
 			return logOutput;
 		}
 
