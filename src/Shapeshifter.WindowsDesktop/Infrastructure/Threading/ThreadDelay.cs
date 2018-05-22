993828c7eb2feab47f58dd187b773383c68b44c9
Mathias Lorenzen <mathias.lorenzen@live.com>
2018-05-22 17:29:59 +0200

    fix for scenario where delay was negative.

--- src/Shapeshifter.WindowsDesktop/Infrastructure/Threading/ThreadDelay.cs
+++ src/Shapeshifter.WindowsDesktop/Infrastructure/Threading/ThreadDelay.cs
@@ -8,12 +8,18 @@
     class ThreadDelay: IThreadDelay
     {
         public async Task ExecuteAsync(int millisecondsDelay)
-        {
+		{
+			if (millisecondsDelay < 1)
+				return;
+
             await Task.Delay(millisecondsDelay);
         }
 
         public void Execute(int millisecondsDelay)
         {
+			if (millisecondsDelay < 1)
+				return;
+
             Thread.Sleep(millisecondsDelay);
         }
     }
