25cd537df693f42252315894fdcf9ef2b2b8262a
Niels Schmidt <it.niels@gmail.com>
2018-09-23 18:15:38 +0200

    Removing compression

--- src/Shapeshifter.WindowsDesktop/Data/Actions/ZipFilesAction.cs
+++ src/Shapeshifter.WindowsDesktop/Data/Actions/ZipFilesAction.cs
@@ -1,145 +1,145 @@
 ﻿namespace Shapeshifter.WindowsDesktop.Data.Actions
 {
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.IO.Compression;
-    using System.Linq;
-    using System.Threading.Tasks;
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Linq;
+	using System.Threading.Tasks;
 
-    using Data.Interfaces;
+	using Data.Interfaces;
 
-    using Infrastructure.Threading.Interfaces;
+	using Infrastructure.Threading.Interfaces;
 
-    using Interfaces;
+	using Interfaces;
 
-    using Services.Clipboard.Interfaces;
-    using Services.Files.Interfaces;
+	using Services.Clipboard.Interfaces;
+	using Services.Files.Interfaces;
 
-    class ZipFilesAction: IZipFilesAction
-    {
-        readonly IAsyncFilter asyncFilter;
-        readonly IFileManager fileManager;
-        readonly IClipboardInjectionService clipboardInjectionService;
+	class ZipFilesAction : IZipFilesAction
+	{
+		readonly IAsyncFilter asyncFilter;
+		readonly IFileManager fileManager;
+		readonly IClipboardInjectionService clipboardInjectionService;
 
-        public ZipFilesAction(
-            IAsyncFilter asyncFilter,
-            IFileManager fileManager,
-            IClipboardInjectionService clipboardInjectionService)
-        {
-            this.asyncFilter = asyncFilter;
-            this.fileManager = fileManager;
-            this.clipboardInjectionService = clipboardInjectionService;
+		public ZipFilesAction(
+			IAsyncFilter asyncFilter,
+			IFileManager fileManager,
+			IClipboardInjectionService clipboardInjectionService)
+		{
+			this.asyncFilter = asyncFilter;
+			this.fileManager = fileManager;
+			this.clipboardInjectionService = clipboardInjectionService;
 		}
 
 		public string Title => "Copy as compressed folder";
 
-        public byte Order => 75;
+		public byte Order => 75;
 
-        public async Task<bool> CanPerformAsync(
-            IClipboardDataPackage package)
-        {
-            var supportedData = await GetSupportedData(package);
-            return supportedData.Any();
-        }
+		public async Task<bool> CanPerformAsync(
+			IClipboardDataPackage package)
+		{
+			var supportedData = await GetSupportedData(package);
+			return supportedData.Any();
+		}
 
-        async Task<IReadOnlyCollection<IClipboardData>> GetSupportedData(
-            IClipboardDataPackage package)
-        {
-            var supportedData = await asyncFilter.FilterAsync(package.Contents, CanPerformAsync);
-            return supportedData;
-        }
+		async Task<IReadOnlyCollection<IClipboardData>> GetSupportedData(
+			IClipboardDataPackage package)
+		{
+			var supportedData = await asyncFilter.FilterAsync(package.Contents, CanPerformAsync);
+			return supportedData;
+		}
 
-        static async Task<bool> CanPerformAsync(
-            IClipboardData data)
-        {
-            return
-                data is IClipboardFileData ||
-                data is IClipboardFileCollectionData;
-        }
+		static async Task<bool> CanPerformAsync(
+			IClipboardData data)
+		{
+			return
+				data is IClipboardFileData ||
+				data is IClipboardFileCollectionData;
+		}
 
-        public async Task PerformAsync(
-            IClipboardDataPackage processedData)
-        {
-            var supportedDataCollection = await GetSupportedData(processedData);
-            var firstSupportedData = supportedDataCollection.FirstOrDefault();
+		public async Task PerformAsync(
+			IClipboardDataPackage processedData)
+		{
+			var supportedDataCollection = await GetSupportedData(processedData);
+			var firstSupportedData = supportedDataCollection.FirstOrDefault();
 
-            var zipFilePath = await ZipDataAsync(firstSupportedData);
-            await clipboardInjectionService.InjectFilesAsync(zipFilePath);
-        }
+			var zipFilePath = await ZipDataAsync(firstSupportedData);
+			await clipboardInjectionService.InjectFilesAsync(zipFilePath);
+		}
 
-        async Task<string> ZipFileCollectionDataAsync(params IClipboardFileData[] fileDataItems)
-        {
-            if (fileDataItems.Length == 0)
-            {
-                throw new ArgumentException(
-                    "There must be at least one item to compress.",
-                    nameof(fileDataItems));
-            }
+		async Task<string> ZipFileCollectionDataAsync(params IClipboardFileData[] fileDataItems)
+		{
+			if (fileDataItems.Length == 0)
+			{
+				throw new ArgumentException(
+					"There must be at least one item to compress.",
+					nameof(fileDataItems));
+			}
 
-            var filePaths = fileDataItems
-                .Select(x => x.FullPath)
-                .ToArray();
-            var commonPath = fileManager.FindCommonFolderFromPaths(filePaths);
-            var directoryName = Path.GetFileName(commonPath);
-            var directoryPath = await fileManager.PrepareTemporaryFolderAsync(directoryName);
-            await CopyFilesToTemporaryFolderAsync(
-                fileDataItems,
-                directoryPath);
+			var filePaths = fileDataItems
+				.Select(x => x.FullPath)
+				.ToArray();
+			var commonPath = fileManager.FindCommonFolderFromPaths(filePaths);
+			var directoryName = Path.GetFileName(commonPath);
+			var directoryPath = await fileManager.PrepareTemporaryFolderAsync(directoryName);
+			await CopyFilesToTemporaryFolderAsync(
+				fileDataItems,
+				directoryPath);
 
-            var zipFile = await ZipDirectoryAsync(directoryPath);
-            return zipFile;
-        }
+			var zipFile = await ZipDirectoryAsync(directoryPath);
+			return zipFile;
+		}
 
-        async Task CopyFilesToTemporaryFolderAsync(
-            IEnumerable<IClipboardFileData> fileDataItems,
-            string directory)
-        {
-            foreach (var fileData in fileDataItems)
-            {
-                await CopyFileToTemporaryFolderAsync(directory, fileData);
-            }
-        }
+		async Task CopyFilesToTemporaryFolderAsync(
+			IEnumerable<IClipboardFileData> fileDataItems,
+			string directory)
+		{
+			foreach (var fileData in fileDataItems)
+			{
+				await CopyFileToTemporaryFolderAsync(directory, fileData);
+			}
+		}
 
-        async Task CopyFileToTemporaryFolderAsync(string directory, IClipboardFileData fileData)
-        {
-            var destinationFilePath = Path.Combine(
-                directory,
-                fileData.FileName);
-            await fileManager.DeleteFileIfExistsAsync(destinationFilePath);
-            File.Copy(fileData.FullPath, destinationFilePath);
-        }
+		async Task CopyFileToTemporaryFolderAsync(string directory, IClipboardFileData fileData)
+		{
+			var destinationFilePath = Path.Combine(
+				directory,
+				fileData.FileName);
+			await fileManager.DeleteFileIfExistsAsync(destinationFilePath);
+			File.Copy(fileData.FullPath, destinationFilePath);
+		}
 
-        async Task<string> ZipDirectoryAsync(string directory)
-        {
-            var directoryName = Path.GetFileName(directory);
-            var compressedFolderDirectory = await fileManager.PrepareTemporaryFolderAsync($"Compressed folders");
-            var zipFile = Path.Combine(compressedFolderDirectory, $"{directoryName}.zip");
+		async Task<string> ZipDirectoryAsync(string directory)
+		{
+			//var directoryName = Path.GetFileName(directory);
+			//var compressedFolderDirectory = await fileManager.PrepareTemporaryFolderAsync($"Compressed folders");
+			//var zipFile = Path.Combine(compressedFolderDirectory, $"{directoryName}.zip");
 
-            await fileManager.DeleteFileIfExistsAsync(zipFile);
-            ZipFile.CreateFromDirectory(directory, zipFile);
+			//await fileManager.DeleteFileIfExistsAsync(zipFile);
+			//ZipFile.CreateFromDirectory(directory, zipFile);
 
-            return zipFile;
-        }
+			//return zipFile;
+			return "";
+		}
 
-        async Task<string> ZipDataAsync(IClipboardData data)
-        {
-            var clipboardFileData = data as IClipboardFileData;
-            if (clipboardFileData != null)
-            {
-                return await ZipFileCollectionDataAsync(clipboardFileData);
-            }
+		async Task<string> ZipDataAsync(IClipboardData data)
+		{
+			var clipboardFileData = data as IClipboardFileData;
+			if (clipboardFileData != null)
+			{
+				return await ZipFileCollectionDataAsync(clipboardFileData);
+			}
 
-            var clipboardFileCollectionData = data as IClipboardFileCollectionData;
-            if (clipboardFileCollectionData != null)
-            {
-                return await ZipFileCollectionDataAsync(
-                    clipboardFileCollectionData
-                        .Files
-                        .ToArray());
-            }
+			var clipboardFileCollectionData = data as IClipboardFileCollectionData;
+			if (clipboardFileCollectionData != null)
+			{
+				return await ZipFileCollectionDataAsync(
+					clipboardFileCollectionData
+						.Files
+						.ToArray());
+			}
 
-            throw new InvalidOperationException("Unknown data format.");
-        }
-    }
+			throw new InvalidOperationException("Unknown data format.");
+		}
+	}
 }
