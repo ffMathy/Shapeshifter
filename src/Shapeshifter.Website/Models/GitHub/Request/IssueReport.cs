4c6372ee83ea3d33a6c4a3018d55ee752e9df3bb
Niels Schmidt <it.niels@gmail.com>
2018-09-23 21:42:29 +0200

    Removed Models project

---
+++ src/Shapeshifter.Website/Models/GitHub/Request/IssueReport.cs
@@ -0,0 +1,15 @@
+﻿namespace Shapeshifter.Website.Models.GitHub.Request
+{
+	public class IssueReport
+	{
+		public SerializableException Exception { get; set; }
+
+		public string OffendingLogLine { get; set; }
+		public string OffendingLogMessage { get; set; }
+		public string Context { get; set; }
+
+		public string Version { get; set; }
+
+		public string[] RecentLogLines { get; set; }
+	}
+}
---
+++ src/Shapeshifter.Website/Models/GitHub/Request/SerializableException.cs
@@ -0,0 +1,9 @@
+﻿namespace Shapeshifter.Website.Models.GitHub.Request
+{
+    public class SerializableException
+	{
+		public string Message { get; set; }
+		public string Name { get; set; }
+		public string StackTrace { get; set; }
+	}
+}
